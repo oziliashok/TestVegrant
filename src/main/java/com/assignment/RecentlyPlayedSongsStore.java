fc19da8cbfe454fa2773dffd5d19ad91d7a28755
unknown <ozili.ashok>
2023-07-31 22:27:03 +0530

    first commit

---
+++ src/main/java/com/assignment/RecentlyPlayedSongsStore.java
@@ -0,0 +1,39 @@
+package com.assignment;
+import java.util.*;
+
+public class RecentlyPlayedSongsStore {
+    private final int initialCapacity;
+    private final HashMap<String, Queue<String>> userSongsMap;
+
+    public RecentlyPlayedSongsStore(int initialCapacity) {
+        this.initialCapacity = initialCapacity;
+        this.userSongsMap = new HashMap<>();
+    }
+
+    public void addRecentlyPlayedSong(String user, String song) {
+        if (!userSongsMap.containsKey(user)) {
+            userSongsMap.put(user, new LinkedList<String>());
+        
+        }
+
+        Queue<String> songsQueue = userSongsMap.get(user);
+
+        // Remove the song from the queue if it already exists, so it can be added to the front
+        songsQueue.remove(song);
+        songsQueue.add(song);
+
+        // If the queue size exceeds the initial capacity, remove the least recently played song
+        if (songsQueue.size() > initialCapacity) {
+            songsQueue.poll();
+        }
+    }
+
+    public List<String> getRecentlyPlayedSongs(String user) {
+        if (!userSongsMap.containsKey(user)) {
+            return Collections.emptyList();
+        }
+
+        // Return a copy of the queue to avoid external modification
+        return new ArrayList<>(userSongsMap.get(user));
+    }
+}
---
+++ src/main/java/com/assignment/RecentlyPlayedSongsStoreTest.java
@@ -0,0 +1,53 @@
+package com.assignment;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class RecentlyPlayedSongsStoreTest {
+    private RecentlyPlayedSongsStore store;
+
+    @BeforeMethod
+    public void setup() {
+        store = new RecentlyPlayedSongsStore(3);
+    }
+
+    @Test
+    public void testAddRecentlyPlayedSongs() {
+        store.addRecentlyPlayedSong("user1", "S1");
+        store.addRecentlyPlayedSong("user1", "S2");
+        store.addRecentlyPlayedSong("user1", "S3");
+
+        List<String> expectedSongs = Arrays.asList("S1", "S2", "S3");
+        List<String> actualSongs = store.getRecentlyPlayedSongs("user1");
+        Assert.assertEquals(actualSongs, expectedSongs);
+
+        store.addRecentlyPlayedSong("user1", "S4");
+
+        expectedSongs = Arrays.asList("S2", "S3", "S4");
+        actualSongs = store.getRecentlyPlayedSongs("user1");
+        Assert.assertEquals(actualSongs, expectedSongs);
+
+        store.addRecentlyPlayedSong("user1", "S2");
+
+        expectedSongs = Arrays.asList("S3", "S4", "S2");
+        actualSongs = store.getRecentlyPlayedSongs("user1");
+        Assert.assertEquals(actualSongs, expectedSongs);
+
+        store.addRecentlyPlayedSong("user1", "S1");
+
+        expectedSongs = Arrays.asList("S4", "S2", "S1");
+        actualSongs = store.getRecentlyPlayedSongs("user1");
+        Assert.assertEquals(actualSongs, expectedSongs);
+    }
+
+    @Test
+    public void testGetRecentlyPlayedSongsNonExistentUser() {
+        List<String> songs = store.getRecentlyPlayedSongs("non_existent_user");
+        Assert.assertTrue(songs.isEmpty());
+    }
+}
